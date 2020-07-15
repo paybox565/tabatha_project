6a3b6ec31bbce10a5523b7b79a40942e233f2895
Vygovskii Oleg <vygovskii@yandex.ru>
2020-07-15 19:01:15 +0500

    Feature: tree component and mock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PartFormComponent } from './part-form/part-form.component';
+import { PositionFormComponent } from './position-form/position-form.component';
+import { PartsListComponent } from './parts-list/parts-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartFormComponent,
+    PositionFormComponent,
+    PartsListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/part-form/part-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-part-form',
+  templateUrl: './part-form.component.html',
+  styleUrls: ['./part-form.component.scss']
+})
+export class PartFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
